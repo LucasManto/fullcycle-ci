a3eb12cad5228f72e4a8e8390529a9e8452d7bc6
Lucas Mantovani <lucashmantovani@hotmail.com>
2022-11-23 12:27:53 -0300

    Update index.spec.js

--- index.spec.js
+++ index.spec.js
@@ -3,7 +3,7 @@ const { sum } = require(".");
 describe('sum', () => {
   it('should return 3', () => {
     const a = 1, b = 2;
-    const expected = 4;
+    const expected = 3;
     const result = sum(a, b);
     expect(result).toEqual(expected);
   })
